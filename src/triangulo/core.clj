b3090bb6e107938c0c5cfb73d0fc529625f45ec3
Luiz Damasceno <luiz.damasceno@luiz.damasceno>
2022-09-16 18:30:51 -0300

    feat: add calculation functions

--- src/triangulo/core.clj
+++ src/triangulo/core.clj
@@ -9,53 +9,65 @@
 (defn calc-radianos
   "TODO: Calcular radianos dado lados a b e c de um triangulo"
   [a b c]
-  )
+  (Math/acos (/ (- (+ (* b b) (* c c)) (* a a)) (* 2 b c))))
 
+; acos((b2 + c2 - a2)/(2*b*c));
 (defn calc-angulo
   "TODO: Calcula o ângulo ∠A, dado A B C."
   [a b c]
-  )
+  (/ (* 180 (calc-radianos a b c)) Math/PI ))
 
 (defn calc-area
   "TODO: Calcula a área de um triângulo usando a formula de Heron."
   [a b c]
-  )
+  (let [p (/ (+ a b c) 2)]
+    (Math/sqrt (* p (* (- p a) (* (- p b) (- p c)))))))
 
+; Area = B * altura / 2
 (defn calc-altura
   "TODO: Calcula altura de A, dado a AREA."
   [a area]
-  )
+  (/ (* 2 area) a))
 
 (defn equilateral?
   "TODO: Verifica se o triangulo é equilateral"
   [a b c]
-  )
+  (= a b c))
 
 (defn isosceles?
   "TODO: Verifica se pelo menos dois lados sao iguais."
   [a b c]
-  )
+  (or (= a b) (= b c) (= a c)))
 
 (defn escaleno?
   "TODO: Verifica se os lados dos triangulos sao diferentes entre si."
   [a b c]
-  )
+  (not (isosceles? a b c)))
 
 (defn retangulo?
   "TODO: Verifica se é um triangulo retangulo, cujos angulos são iguais a 90o.
   O resultado não é exato, dado que cada angulo é arredondado utilizando clojure.math/round."
   [a b c]
-  )
+  (let [angulo-1 (calc-angulo a b c)
+        angulo-2 (calc-angulo b a c)
+        angulo-3 (calc-angulo c b a)]
+    (boolean (some #(== 90 %) [angulo-1 angulo-2 angulo-3]))))
 
 (defn obtuso?
   "TODO: Verifica se o triangulo é obtuso, tendo algum angulo >90o."
   [a b c]
-  )
+  (let [angulo-1 (calc-angulo a b c)
+        angulo-2 (calc-angulo b a c)
+        angulo-3 (calc-angulo c b a)]
+    (boolean (some #(> % 90) [angulo-1 angulo-2 angulo-3]))))
 
 (defn agudo?
   "TODO: Verifica se o triangulo é obtuso, tendo algum angulo >90o."
   [a b c]
-  )
+  (let [angulo-1 (calc-angulo a b c)
+        angulo-2 (calc-angulo b a c)
+        angulo-3 (calc-angulo c b a)]
+    (boolean (every? #(< % 90) [angulo-1 angulo-2 angulo-3]))))
 
 (defn gerar-dados-completos
   [a b c]
@@ -74,6 +86,7 @@
          :angulos [(calc-angulo a b c)
                    (calc-angulo b c a)
                    (calc-angulo c a b)]}))
+
 
 (comment
   (require 'clojure.pprint)
